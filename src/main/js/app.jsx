b7295fa10c3e8a05f84774b8fe0610d3a20f42a5
Zhigalov-E <zhigalov_java@mail.ru>
2018-03-28 04:28:13 +0300

    render FileList

--- src/main/js/app.jsx
+++ src/main/js/app.jsx
@@ -5,44 +5,94 @@ class App extends React.Component {
 
     constructor() {
         super();
+        this.showFileStatistic = this.showFileStatistic.bind(this);
+        this.hideFileStatistic = this.hideFileStatistic.bind(this);
         this.state = {files: []};
+        this.isShowFileStat = {isShowFileStat: false};
     }
 
     componentDidMount() {
-        fetch(`http://localhost:8080/api/files`)
+        fetch(`/api/files`)
             .then(result => result.json())
             .then(files => this.setState({files}))
+            .catch((err) => {
+                console.error('err', err);
+            });
+    }
+
+    showFileStatistic(id) {
+        this.setState({isShowFileStat: true});
+        this.setState({fileId: id})
+    }
+
+    hideFileStatistic() {
+        this.setState({isShowFileStat: false});
     }
 
     render() {
-        return (<FileList files={this.state.files}/>);
+        return (
+            <div>
+                <span className="text-center ">
+                    <h5>File Statistic</h5>
+                </span>
+                <div className="row">
+                    <div className="col-md-5">
+                        <FileList files={this.state.files}
+                                  show={this.showFileStatistic}
+                                  hide={this.hideFileStatistic}/>
+                    </div>
+                    <div className="col-md-5">
+                        {  this.state.isShowFileStat &&
+                        <FileStatistic fileId={this.state.fileId}/>
+                        }
+                    </div>
+                </div>
+            </div>
+
+        );
     }
 };
 
 class FileList extends React.Component {
     render() {
         var files = this.props.files.map((file, i) =>
-            <File key={i} file={file}/>
+            <File key={i} file={file} show={this.props.show} hide={this.props.hide}/>
         );
         return (
-            <div className="container">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>FileName</th>
-                            <th>Path</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>{files}</tbody>
-                </table>
-            </div>
+            <table className="table table-bordered left">
+                <thead>
+                <tr>
+                    <th className="col-md-1">ID</th>
+                    <th className="col-md-2">FileName</th>
+                    <th className="col-md-3">Path</th>
+                    <th className="col-md-2"></th>
+                </tr>
+                </thead>
+                <tbody>{files}</tbody>
+            </table>
         );
     }
 };
 
 class File extends React.Component {
+    constructor() {
+        super();
+        this.clickHandler = this.clickHandler.bind(this);
+        this.state = {isShowDetail: false};
+        this.state = {buttonName: "Show detail"};
+    }
+
+    clickHandler() {
+        this.setState({isShowDetail: !this.state.isShowDetail});
+        if (this.state.isShowDetail) {
+            this.setState({buttonName: "Hide detail"});
+            this.props.show(this.props.file.id);
+        } else {
+            this.setState({buttonName: "Show detail"});
+            this.props.hide();
+        }
+    };
+
     render() {
         return (
             <tr key={this.props.file.id}>
@@ -50,7 +100,8 @@ class File extends React.Component {
                 <td>{this.props.file.name}</td>
                 <td>{this.props.file.path}</td>
                 <td>
-                    <button className="btn btn-info">Show detail</button>
+                    <button className="btn btn-info"
+                            onClick={this.clickHandler}>{this.state.buttonName}</button>
                 </td>
             </tr>
         );
@@ -60,84 +111,50 @@ class File extends React.Component {
 class FileStatistic extends React.Component {
     constructor() {
         super();
+        this.state = {fileStat: []};
+    }
+
+    componentDidMount() {
+        fetch(`/api/files/` + this.props.fileId + '/stat')
+            .then(result => result.json())
+            .then(fileStat => this.setState({fileStat}))
+            .catch((err) => {
+                console.error('err', err);
+            });
+
+    }
+
+    eachItem(item, i) {
+        return (
+            <tr key={item.id}>
+                <th>{item.longestWord}</th>
+                <th>{item.shortestWord}</th>
+                <th>{item.linesLength}</th>
+                <th>{item.averageWordLength}</th>
+            </tr>
+        );
     }
 
     render() {
-        return(
-            <div className="container">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>LongestWord</th>
-                            <th>ShortestWord</th>
-                            <th>LinesLength</th>
-                            <th>AverageWordLength</th>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
-            </div>
-        )
-    }
-
-    onClick() {
-        this.setState({fileStatVisible: !this.state.fileStatVisible});
+        return (
+            <table className="table table-bordered right">
+                <thead>
+                <tr>
+                    <th>LongestWord</th>
+                    <th>ShortestWord</th>
+                    <th>LinesLength</th>
+                    <th>AverageWordLength</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.fileStat.map(this.eachItem)}
+                </tbody>
+            </table>
+        );
     }
 };
-
 
 
 ReactDOM.render(<App/>,
     document.getElementById('react')
 );
-
-
-/*
-var cols = [
-    { key: 'firstName', label: 'First Name' },
-    { key: 'lastName', label: 'Last Name' }
-];
-
-var data = [
-    { id: 1, firstName: 'John', lastName: 'Doe' },
-    { id: 2, firstName: 'Clark', lastName: 'Kent' }
-];
-
-var Table = React.createClass({
-
-    render: function() {
-        var headerComponents = this.generateHeaders(),
-            rowComponents = this.generateRows();
-
-        return (
-            <table>
-                <thead> {headerComponents} </thead>
-                <tbody> {rowComponents} </tbody>
-            </table>
-        );
-    },
-
-    generateHeaders: function() {
-        var cols = this.props.cols;  // [{key, label}]
-
-        // generate our header (th) cell components
-        return cols.map(function(colData) {
-            return <th key={colData.key}> {colData.label} </th>;
-        });
-    },
-
-    generateRows: function() {
-        var cols = this.props.cols,  // [{key, label}]
-            data = this.props.data;
-
-        return data.map(function(item) {
-            // handle the column data within each row
-            var cells = cols.map(function(colData) {
-
-                // colData.key might be "firstName"
-                return <td> {item[colData.key]} </td>;
-            });
-            return <tr key={item.id}> {cells} </tr>;
-        });
-    }
-});*/
